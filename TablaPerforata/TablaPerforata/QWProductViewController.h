52c841802f3f8e75f0705072fc482b92389e4a4d
Dan Timofte <timoftedan@gmail.com>
2013-04-22 19:10:46 +0300

    product

--- TablaPerforata/TablaPerforata/QWProductViewController.h
+++ TablaPerforata/TablaPerforata/QWProductViewController.h
@@ -7,7 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MWPhotoBrowser.h"
 
-@interface QWProductViewController : UIViewController
+@interface QWProductViewController : UIViewController <MWPhotoBrowserDelegate> {
+    NSArray *_photos;
+    UISegmentedControl *_segmentedControl;
+    MWPhotoBrowser *cbrowser;
+    UIImageView *firstImage;
+}
+@property (nonatomic, retain) NSArray *photos;
+@property (nonatomic, retain) MWPhotoBrowser *cbrowser;
+@property (nonatomic,retain) IBOutlet UIImageView *firstImage;
 
 @end
--- TablaPerforata/TablaPerforata/QWProductViewController.m
+++ TablaPerforata/TablaPerforata/QWProductViewController.m
@@ -7,33 +7,101 @@
 //
 
 #import "QWProductViewController.h"
-
 @interface QWProductViewController ()
 
 @end
 
 @implementation QWProductViewController
 
+@synthesize photos = _photos;
+@synthesize firstImage,cbrowser;
+
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
+    NSLog(@"here i am");
+
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+		self.title = @"MWPhotoBrowser";
+        
+        _segmentedControl = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"Push", @"Modal", nil]];
+        _segmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
+        _segmentedControl.selectedSegmentIndex = 0;
+        [_segmentedControl addTarget:self action:@selector(segmentChange) forControlEvents:UIControlEventValueChanged];
+        
+        UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithCustomView:_segmentedControl];
+        self.navigationItem.rightBarButtonItem = item;
+        self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleBordered target:nil action:nil];
     }
     return self;
+    
 }
 
 - (void)viewDidLoad
 {
     UIColor* bgColor = [UIColor colorWithPatternImage:[UIImage tallImageNamed:@"bg.png"]];
     [self.view setBackgroundColor:bgColor];
+    firstImage.image = [UIImage imageNamed:@"photo1l.jpg"];
+    // Browser
+	NSMutableArray *photos = [[NSMutableArray alloc] init];
+    MWPhoto *photo;
+    photo = [MWPhoto photoWithFilePath:[[NSBundle mainBundle] pathForResource:@"photo1l" ofType:@"jpg"]];
+    photo.caption = @"Grotto of the Madonna";
+    [photos addObject:photo];
+    photo = [MWPhoto photoWithFilePath:[[NSBundle mainBundle] pathForResource:@"photo2l" ofType:@"jpg"]];
+    photo.caption = @"The London Eye is a giant Ferris wheel situated on the banks of the River Thames, in London, England.";
+    [photos addObject:photo];
+    photo = [MWPhoto photoWithFilePath:[[NSBundle mainBundle] pathForResource:@"photo3l" ofType:@"jpg"]];
+    photo.caption = @"York Floods";
+    [photos addObject:photo];
+    photo = [MWPhoto photoWithFilePath:[[NSBundle mainBundle] pathForResource:@"photo4l" ofType:@"jpg"]];
+    photo.caption = @"Campervan";
+    [photos addObject:photo];
+    self.photos = photos;
+	
+	// Create browser
+	MWPhotoBrowser *browser = [[MWPhotoBrowser alloc] initWithDelegate:self];
+    browser.displayActionButton = YES;
+    //browser.wantsFullScreenLayout = NO;
+    //[browser setInitialPageIndex:2];
+    self.cbrowser=browser;
+    //UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:browser];
+    //nc.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+    //[self presentViewController:nc animated:YES completion:nil];
+    
     [super viewDidLoad];
     
 	// Do any additional setup after loading the view.
 }
 
 
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    UITouch *touch = [touches anyObject];
+    
+    if ([touch view] == firstImage)
+    {
+        UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:self.cbrowser];
+        nc.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+        [self presentViewController:nc animated:YES completion:nil];
+        //add your code for image touch here
+    }
+    
+}
 
+
+#pragma mark - MWPhotoBrowserDelegate
+
+- (NSUInteger)numberOfPhotosInPhotoBrowser:(MWPhotoBrowser *)photoBrowser {
+    return _photos.count;
+}
+
+- (MWPhoto *)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index {
+    if (index < _photos.count)
+        return [_photos objectAtIndex:index];
+    return nil;
+}
 
 - (void)didReceiveMemoryWarning
 {
